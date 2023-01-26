510d0138cf6f631a6a720881e94c11f967dcd7ca
Raul Gonzalez <rauls.gh24@gmail.com>
2023-01-25 18:31:35 -0600

    Memorama

---
+++ src/pages/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+import Card from '../components/Card/Card';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="cards">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
+}
+
+export default App;
