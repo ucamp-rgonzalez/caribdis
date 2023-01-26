7986ed67e425c1de8ec2fda67ce0fd47c4a301ee
Raul Gonzalez <rauls.gh24@gmail.com>
2023-01-25 21:35:55 -0600

    Solved

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,15 @@
+import { useState } from 'react';
+
 import './Card.css';
 
-function Card() {
-  return <div className="card">?</div>;
+function Card(props) {
+  const { matched, onClick, hide, value } = props;
+
+  return (
+    <div className="card" onClick={onClick}>
+      {matched || !hide ? value : '?'}
+    </div>
+  );
 }
 
 export default Card;
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,23 +1,61 @@
+import { useEffect, useState } from 'react';
+
 import './App.css';
 
 import Card from '../components/Card/Card';
 
 function App() {
+  const [values, setValues] = useState([]);
+
+  useEffect(() => {
+    const shuffled = ['a', 'b', 'c', 'd', 'e', 'f']
+      .reduce((prev, curr) => {
+        prev.push(curr, curr);
+        return prev;
+      }, [])
+      .sort((a, b) => 0.5 - Math.random())
+      .map((letter) => ({ value: letter, hide: true, matched: false }));
+    setValues(shuffled);
+  }, []);
+
+  console.log(values);
+
+  function handleClick({ value, hide, matched }, index) {
+    const hasHiddenFalse = values.findIndex((value) => value.hide === false);
+
+    if (hasHiddenFalse !== -1) {
+      const previous = values[hasHiddenFalse];
+
+      if (previous.value === value) {
+        values[hasHiddenFalse].hide = undefined;
+        values[hasHiddenFalse].matched = true;
+        values[index].hide = undefined;
+        values[index].matched = true;
+        setValues([...values]);
+        return;
+      }
+      values[hasHiddenFalse].hide = true;
+      values[index].hide = true;
+      setValues([...values]);
+      return;
+    }
+
+    values[index].hide = false;
+    setValues([...values]);
+  }
+
   return (
     <div className="container">
       <div className="cards">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {values.map(({ value, hide, matched }, index) => (
+          <Card
+            key={index}
+            value={value}
+            hide={hide}
+            onClick={() => handleClick({ value, hide, matched }, index)}
+            matched={matched}
+          />
+        ))}
       </div>
     </div>
   );
